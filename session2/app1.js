844addae1882a2513428c14dd80d09aeaf50907f
unknown <nithyabmc26@gmail.com>
2019-10-26 14:11:45 +0530

    saturday practice

---
+++ session2/app1.js
@@ -0,0 +1,7 @@
+function myFunction (){
+    //console.log(event)
+    //debugger
+  // element.style.color="red"
+  document.getElementById("sjcit").innerHTML=event.target.value
+    console.log(event.target.value)
+} 
